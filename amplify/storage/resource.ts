4a864423492d1a986216e6dff355d7c38ea495d8
jaimeardp <jaimeardp@gmail.com>
2024-08-08 17:12:36 -0500

    chore: Update file path for image upload in GenPost component

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,10 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'amplifyTeamDrive'
+  name: 'amplifyTeamDrive',
+  access: (allow) => ({
+    'profile/*': [
+      allow.guest.to(['read', 'write']) // additional actions such as "write" and "delete" can be specified depending on your use case
+    ]
+  })
 });
